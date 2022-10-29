46dd4829a8ae500c3c5ccbd8171848690759817c
Sara L <wwwsaramilk@gmail.com>
2022-10-29 09:21:11 -0300

    create pages/routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from "styled-components"; 
-import { Button } from "./components/Button";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./components/Router";
+
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themps/default";
 
 export function App() {
   return (
     <ThemeProvider theme= {defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="sucess" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
