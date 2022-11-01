45fac0df8ae5503a53f8fe71728969af3424fe81
Sara L <wwwsaramilk@gmail.com>
2022-10-31 23:27:24 -0300

    component status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
     return (
@@ -20,37 +20,49 @@ export function History() {
                             <td>Task</td>
                             <td>20 minutes</td>
                             <td>2 months ago</td>
-                            <td>Fineshed</td>
+                            <td>
+                                <Status statusColor="green">finished</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Task</td>
                             <td>20 minutes</td>
                             <td>2 months ago</td>
-                            <td>Fineshed</td>
+                            <td>
+                                <Status statusColor="red">interrupted</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Task</td>
                             <td>20 minutes</td>
                             <td>2 months ago</td>
-                            <td>Fineshed</td>
+                            <td>
+                                <Status statusColor="green">finished</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Task</td>
                             <td>20 minutes</td>
                             <td>2 months ago</td>
-                            <td>Fineshed</td>
+                            <td>
+                                <Status statusColor="yellow">ongoing</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Task</td>
                             <td>20 minutes</td>
                             <td>2 months ago</td>
-                            <td>Fineshed</td>
+                            <td>
+                                <Status statusColor="yellow">ongoing</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Task</td>
                             <td>20 minutes</td>
                             <td>2 months ago</td>
-                            <td>Fineshed</td>
+                            <td>
+                                <Status statusColor="green">Finished</Status>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
