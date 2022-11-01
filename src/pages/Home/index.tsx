b5ee2932cab6530610e835f49b5b5a0baf139793
Sara L <wwwsaramilk@gmail.com>
2022-10-31 22:12:31 -0300

    improving inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,14 +9,24 @@ export function Home() {
                     <label htmlFor="task">i'll start in</label>
                     <TaskInput 
                         id="task" 
+                        list="task-suggestions"
                         placeholder="give a name to your project" 
                     />
+
+                    <datalist id="task-suggestions">
+                        <option value="Project 1" />
+                        <option value="Project 2" />
+                        <option value="Project 3" />
+                    </datalist>
 
                     <label htmlFor="minutesAmount">during</label>
                     <MinuteAmountInput 
                         type="number" 
                         id="minutesAmount" 
                         placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
                     />
 
                     <span>minutes</span>
