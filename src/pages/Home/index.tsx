8cb16df39d7fea753777340399568592f4f4ff23
Sara L <wwwsaramilk@gmail.com>
2022-11-04 21:19:08 -0300

    starting new cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import { Play } from "phosphor-react";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as zod from 'zod';
+import { useState } from "react";
 
 import { CountdownContainer, FormContainer, HomeContainer, MinuteAmountInput, Separator, StartCountDownButton, TaskInput } from "./styles";
 
@@ -17,8 +18,18 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+
+interface Cycle {
+    id: string
+    task: string
+    minutesAmount: number
+}
+
 export function Home() {
-    const { register, handleSubmit, watch} = useForm<NewCycleFormData>({
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+    const { register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
             task: '',
@@ -27,8 +38,19 @@ export function Home() {
     })
 
     function handleCreateNewCycle(data: NewCycleFormData) {
-        console.log(data)
+        const newCycle: Cycle = {
+            id: String(new Date().getTime()),
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+        }
+
+        setCycles((state) => [...cycles, newCycle])
+        setActiveCycleId(id)
+
+        reset();
     }
+
+    const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId)
 
     const task = watch('task')
     const isSubmitDisableb = !task
